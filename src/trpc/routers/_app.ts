e8c1be3f8e88de0d442d5ce942e7a91d0dde86da
Rohit Rai <161055240+Rohitrai12@users.noreply.github.com>
2025-11-09 19:58:34 +0500

    background jobs with inngest

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,13 +1,18 @@
 import { z } from 'zod';
-import { baseProcedure, createTRPCRouter, protectProcedure } from '../init';
+import { createTRPCRouter, protectProcedure } from '../init';
 import { prisma } from '@/lib/db';
 export const appRouter = createTRPCRouter({
- getUsers: protectProcedure.query(async ({ ctx }) => {
-  console.log({ userId: ctx.user.id });
-  return prisma.user.findMany({
-    where: { id: ctx.user.id },
-  });
+ getWorkflows: protectProcedure.query(async ({ ctx }) => {
+  return prisma.workflow.findMany({});
+ }),
 
+ createWorkflow: protectProcedure.mutation(()=>{
+  return prisma.workflow.create({
+    data: {
+      name: 'test'
+    }
+  })
+  
  })
 });
 // export type definition of API
